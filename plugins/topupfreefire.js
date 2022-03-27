ca58d866e7ffdadc14d5d262fdc43e6ceda004bf
Zura88 <agungyudisaputro@gmail.com>
2022-03-27 22:05:04 +0700

    zurra

---
+++ plugins/topupfreefire.js
@@ -0,0 +1,32 @@
+const topupfreefire = (prefix) => { 
+	return `*HALLO KAK👋*
+*PILIH DIBAWAH YA!!!*
+*LIST DM FF*
+*NO ILEGAL 100%*
+5.500 = 20💎
+9.500 = 50💎
+11.500 = 70💎
+17.500 = 100💎
+21.500 = 140💎
+32.500 = 210💎
+52.500 = 355💎
+77.500 = 510💎
+105.300 = 720💎
+205.300 = 1450💎
+311.200 = 2180💎
+510.600 = 3640💎
+1.050.100 = 17280💎
+*MM* : 29.300
+*MB*  : 141.600
+_*PEMBAYARAN*_
+
+085701662971 (DANA, OVO, & GOPAY)
+*An. Agung Yudisaputro*
+
+4220261404 (BCA)
+*An. Agung Yudisaputro*
+*minat? hub :* http://wa.me/${nomorowner}
+`
+}
+
+exports.topupfreefire = topupfreefire
---
+++ plugins/topuppubg.js
@@ -0,0 +1,20 @@
+const topuppubg = (prefix) => { 
+	return `*HALLO KAK👋*
+*PILIH DIBAWAH YA!!!*
+*LIST UC PUBG*
+*NO ILEGAL*
+- 🎟️60 = Rp14.000
+- 🎟️120 = Rp28.000
+- 🎟️240 = Rp56.000
+_*PEMBAYARAN*_
+
+085701662971 (DANA, OVO, & GOPAY)
+*An. Agung Yudisaputro*
+
+4220261404 (BCA)
+*An. Agung Yudisaputro*
+*minat? hub :* http://wa.me/${nomorowner}
+`
+}
+
+exports.topuppubg = topuppubg
